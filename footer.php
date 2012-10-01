f6fda6883126d92d1cbe7a4ed83aa31375f58a80
Eric Kiel <eric.kiel@collideoscope.org>
2012-10-01 12:47:17 -0500

    adding first version

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div>
+<div id="ft">
+<?php wp_footer(); ?>
+</div>
+</div><!-- End id=doc -->
+</div><!-- End id=wrapper -->
+<div class="clear">&nbsp;</div>
+<div id="footnote" align="center">
+	Powered by <a href="http://wordpress.org/">Wordpress</a> and the <a href="http://collideoscope.org/projects/opennote-wordpress-theme/">OpenNote WordPress Theme</a>.
+</div>
+</div><!-- End align=center -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<div id="bd" class="index">
+<div id="yui-main"><div class="yui-b">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div class="post-wrap" id="post-<?php the_ID(); ?>">
+		<h1 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title(); ?>"><?php the_title(); ?></a></h1>
+	
+		
+		<div class="post-meta">
+			<?php edit_post_link('Edit', '', ' |'); ?> <?php the_tags('tags: ', ', ', '<br />'); ?>
+			
+			
+		</div>
+	</div><!-- end post -->
+	<?php endwhile; ?>
+		<div class="nav">
+			<div class="previous"><?php next_posts_link('&laquo; Previous') ?></div>
+			<div class="next"><?php previous_posts_link('Next &raquo;') ?></div>
+		</div>
+	<?php else : ?>
+		<h2 class="post-title"><?php _e('Not Found'); ?></h2>
+		<p><?php _e("Sorry, but you are looking for something that isn't here. Double check your URL or you should try searching for it."); ?></p>
+		<?php @include (TEMPLATEPATH . "/searchform.php"); ?>
+<?php endif; ?>
+</div>
+</div>
+
+<?php get_footer(); ?>
